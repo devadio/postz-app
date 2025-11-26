7341ed98ab857ca13c9dad62a9874d03c03d33a2
Gharib_Shaban26 <165028759+Gcodesh@users.noreply.github.com>
2025-11-26 16:54:58 +0200

    Add Logo component to display the logo image

---
+++ apps/frontend/src/components/new-layout/logo.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export const Logo = () => {
+  return (
+    <img
+      src="/postiz-text.svg"
+      alt="Logo"
+      width={60}
+      height={60}
+      className="mt-[8px]"
+    />
+  );
+};
