161df61b63d35910bffadd28d5611082971fcec3
Gharib_Shaban26 <165028759+Gcodesh@users.noreply.github.com>
2025-11-27 00:23:42 +0200

    Refactor layout.settings.tsx for cleaner code
    Refactor imports and simplify variable declarations. Remove unused imports and components.

--- apps/frontend/src/components/layout/layout.settings.tsx
+++ apps/frontend/src/components/layout/layout.settings.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import { ReactNode, useCallback, useEffect } from 'react';
-import { Title } from '@gitroom/frontend/components/layout/title';
+import { ReactNode, useCallback } from 'react';
 import { ContextWrapper } from '@gitroom/frontend/components/layout/user.context';
 import { TopMenu } from '@gitroom/frontend/components/layout/top.menu';
 import { MantineWrapper } from '@gitroom/react/helpers/mantine.wrapper';
 import { ToolTip } from '@gitroom/frontend/components/layout/top.tip';
 import { ShowMediaBoxModal } from '@gitroom/frontend/components/media/media.component';
 import Image from 'next/image';
-import { Toaster, useToaster } from '@gitroom/react/toaster/toaster';
+import { Toaster } from '@gitroom/react/toaster/toaster';
 import { ShowPostSelector } from '@gitroom/frontend/components/post-url-selector/post.url.selector';
 import { OrganizationSelector } from '@gitroom/frontend/components/layout/organization.selector';
 import NotificationComponent from '@gitroom/frontend/components/notifications/notification.component';
@@ -21,44 +20,39 @@ import isoWeek from 'dayjs/plugin/isoWeek';
 import isBetween from 'dayjs/plugin/isBetween';
 import { ShowLinkedinCompany } from '@gitroom/frontend/components/launches/helpers/linkedin.component';
 import { SettingsComponent } from '@gitroom/frontend/components/layout/settings.component';
-import { Onboarding } from '@gitroom/frontend/components/onboarding/onboarding';
-import { Support } from '@gitroom/frontend/components/layout/support';
 import { ContinueProvider } from '@gitroom/frontend/components/layout/continue.provider';
 import { CopilotKit } from '@copilotkit/react-core';
 import { Impersonate } from '@gitroom/frontend/components/layout/impersonate';
-import clsx from 'clsx';
 import { BillingComponent } from '@gitroom/frontend/components/billing/billing.component';
 import dynamic from 'next/dynamic';
 import { NewSubscription } from '@gitroom/frontend/components/layout/new.subscription';
 import { useVariables } from '@gitroom/react/helpers/variable.context';
-const ModeComponent = dynamic(
-  () => import('@gitroom/frontend/components/layout/mode.component'),
-  {
-    ssr: false,
-  }
-);
 import { extend } from 'dayjs';
 import { useSearchParams } from 'next/navigation';
 import { CheckPayment } from '@gitroom/frontend/components/layout/check.payment';
 import { ChromeExtensionComponent } from '@gitroom/frontend/components/layout/chrome.extension.component';
 import { LanguageComponent } from '@gitroom/frontend/components/layout/language.component';
-import { useT } from '@gitroom/react/translation/get.transation.service.client';
-import i18next from '@gitroom/react/translation/i18next';
 import { MediaSettingsLayout } from '@gitroom/frontend/components/launches/helpers/media.settings.component';
+
+const ModeComponent = dynamic(
+  () => import('@gitroom/frontend/components/layout/mode.component'),
+  { ssr: false }
+);
+
 extend(utc);
 extend(weekOfYear);
 extend(isoWeek);
 extend(isBetween);
+
 export const LayoutSettings = ({ children }: { children: ReactNode }) => {
   const fetch = useFetch();
-  const t = useT();
-
-  const { isGeneral } = useVariables();
-  const { backendUrl, billingEnabled } = useVariables();
+  const { isGeneral, backendUrl, billingEnabled } = useVariables();
   const searchParams = useSearchParams();
+
   const load = useCallback(async (path: string) => {
     return await (await fetch(path)).json();
   }, []);
+
   const { data: user, mutate } = useSWR('/user/self', load, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
@@ -66,7 +60,9 @@ export const LayoutSettings = ({ children }: { children: ReactNode }) => {
     refreshWhenOffline: false,
     refreshWhenHidden: false,
   });
+
   if (!user) return null;
+
   return (
     <ContextWrapper user={user}>
       <CopilotKit
@@ -85,41 +81,40 @@ export const LayoutSettings = ({ children }: { children: ReactNode }) => {
           <Toaster />
           <ShowPostSelector />
           <NewSubscription />
-          <Support />
           <ContinueProvider />
           <div className="min-h-[100vh] w-full max-w-[1440px] mx-auto bg-primary px-6 text-textColor flex flex-col">
             {user?.admin && <Impersonate />}
             <nav className="flex items-center justify-between">
-            <Link
-  href="/"
-  className="text-2xl flex items-center gap-[10px] text-textColor order-1"
->
-  
-  <div className="min-w-[55px]">
-    <Image
-      src="/postiz.svg"
-      width={55}
-      height={53}
-      alt="Old Logo"
-    />
-  </div>
-  <div className="min-w-[55px]">
-    <Image
-      src="/postiz-text.svg"
-      width={55}
-      height={53}
-      alt="New Logo"
-    />
-  </div>
-</Link>
+              <Link
+                href="/"
+                className="flex items-center gap-[10px] text-textColor order-1"
+              >
+                {/* اللوجو القديم */}
+                <div className="min-w-[55px]">
+                  <Image
+                    src="/postiz.svg"
+                    width={55}
+                    height={53}
+                    alt="Old Logo"
+                  />
+                </div>
 
+                {/* اللوجو الجديد */}
+                <div className="min-w-[55px]">
+                  <Image
+                    src="/postiz-text.svg"
+                    width={55}
+                    height={53}
+                    alt="New Logo"
+                  />
+                </div>
+              </Link>
 
               {user?.orgId &&
               (user.tier !== 'FREE' || !isGeneral || !billingEnabled) ? (
                 <TopMenu />
-              ) : (
-                <></>
-              )}
+              ) : null}
+
               <div
                 id="systray-buttons"
                 className="flex items-center justify-self-end gap-[8px] order-2 md:order-3"
@@ -132,98 +127,13 @@ export const LayoutSettings = ({ children }: { children: ReactNode }) => {
                 <OrganizationSelector />
               </div>
             </nav>
+
             <div className="flex-1 flex">
               <div className="flex-1 rounded-3xl px-0 py-[17px] flex flex-col">
                 {user.tier === 'FREE' && isGeneral && billingEnabled ? (
-                  <>
-                    <div className="text-center mb-[20px] text-xl [@media(max-width:1024px)]:text-xl">
-                      <h1 className="text-3xl [@media(max-width:1024px)]:text-xl">
-                        {t(
-                          'join_10000_entrepreneurs_who_use_postiz',
-                          'Join 10,000+ Entrepreneurs Who Use Postiz'
-                        )}
-                        <br />
-                        {t(
-                          'to_manage_all_your_social_media_channels',
-                          'To Manage All Your Social Media Channels'
-                        )}
-                      </h1>
-                      <br />
-                      {user?.allowTrial && (
-                        <div className="table mx-auto">
-                          <div className="flex gap-[5px] items-center">
-                            <div>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                              >
-                                <path
-                                  d="M16.2806 9.21937C16.3504 9.28903 16.4057 9.37175 16.4434 9.46279C16.4812 9.55384 16.5006 9.65144 16.5006 9.75C16.5006 9.84856 16.4812 9.94616 16.4434 10.0372C16.4057 10.1283 16.3504 10.211 16.2806 10.2806L11.0306 15.5306C10.961 15.6004 10.8783 15.6557 10.7872 15.6934C10.6962 15.7312 10.5986 15.7506 10.5 15.7506C10.4014 15.7506 10.3038 15.7312 10.2128 15.6934C10.1218 15.6557 10.039 15.6004 9.96938 15.5306L7.71938 13.2806C7.57865 13.1399 7.49959 12.949 7.49959 12.75C7.49959 12.551 7.57865 12.3601 7.71938 12.2194C7.86011 12.0786 8.05098 11.9996 8.25 11.9996C8.44903 11.9996 8.6399 12.0786 8.78063 12.2194L10.5 13.9397L15.2194 9.21937C15.289 9.14964 15.3718 9.09432 15.4628 9.05658C15.5538 9.01884 15.6514 8.99941 15.75 8.99941C15.8486 8.99941 15.9462 9.01884 16.0372 9.05658C16.1283 9.09432 16.211 9.14964 16.2806 9.21937ZM21.75 12C21.75 13.9284 21.1782 15.8134 20.1068 17.4168C19.0355 19.0202 17.5127 20.2699 15.7312 21.0078C13.9496 21.7458 11.9892 21.9389 10.0979 21.5627C8.20656 21.1865 6.46928 20.2579 5.10571 18.8943C3.74215 17.5307 2.81355 15.7934 2.43735 13.9021C2.06114 12.0108 2.25422 10.0504 2.99218 8.26884C3.73013 6.48726 4.97982 4.96451 6.58319 3.89317C8.18657 2.82183 10.0716 2.25 12 2.25C14.585 2.25273 17.0634 3.28084 18.8913 5.10872C20.7192 6.93661 21.7473 9.41498 21.75 12ZM20.25 12C20.25 10.3683 19.7661 8.77325 18.8596 7.41655C17.9531 6.05984 16.6646 5.00242 15.1571 4.37799C13.6497 3.75357 11.9909 3.59019 10.3905 3.90852C8.79017 4.22685 7.32016 5.01259 6.16637 6.16637C5.01259 7.32015 4.22685 8.79016 3.90853 10.3905C3.5902 11.9908 3.75358 13.6496 4.378 15.1571C5.00242 16.6646 6.05984 17.9531 7.41655 18.8596C8.77326 19.7661 10.3683 20.25 12 20.25C14.1873 20.2475 16.2843 19.3775 17.8309 17.8309C19.3775 16.2843 20.2475 14.1873 20.25 12Z"
-                                  fill="#06ff00"
-                                />
-                              </svg>
-                            </div>
-                            <div>
-                              {t('100_no_risk_trial', '100% no-risk trial')}
-                            </div>
-                          </div>
-                          <div className="flex gap-[5px] items-center">
-                            <div>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                              >
-                                <path
-                                  d="M16.2806 9.21937C16.3504 9.28903 16.4057 9.37175 16.4434 9.46279C16.4812 9.55384 16.5006 9.65144 16.5006 9.75C16.5006 9.84856 16.4812 9.94616 16.4434 10.0372C16.4057 10.1283 16.3504 10.211 16.2806 10.2806L11.0306 15.5306C10.961 15.6004 10.8783 15.6557 10.7872 15.6934C10.6962 15.7312 10.5986 15.7506 10.5 15.7506C10.4014 15.7506 10.3038 15.7312 10.2128 15.6934C10.1218 15.6557 10.039 15.6004 9.96938 15.5306L7.71938 13.2806C7.57865 13.1399 7.49959 12.949 7.49959 12.75C7.49959 12.551 7.57865 12.3601 7.71938 12.2194C7.86011 12.0786 8.05098 11.9996 8.25 11.9996C8.44903 11.9996 8.6399 12.0786 8.78063 12.2194L10.5 13.9397L15.2194 9.21937C15.289 9.14964 15.3718 9.09432 15.4628 9.05658C15.5538 9.01884 15.6514 8.99941 15.75 8.99941C15.8486 8.99941 15.9462 9.01884 16.0372 9.05658C16.1283 9.09432 16.211 9.14964 16.2806 9.21937ZM21.75 12C21.75 13.9284 21.1782 15.8134 20.1068 17.4168C19.0355 19.0202 17.5127 20.2699 15.7312 21.0078C13.9496 21.7458 11.9892 21.9389 10.0979 21.5627C8.20656 21.1865 6.46928 20.2579 5.10571 18.8943C3.74215 17.5307 2.81355 15.7934 2.43735 13.9021C2.06114 12.0108 2.25422 10.0504 2.99218 8.26884C3.73013 6.48726 4.97982 4.96451 6.58319 3.89317C8.18657 2.82183 10.0716 2.25 12 2.25C14.585 2.25273 17.0634 3.28084 18.8913 5.10872C20.7192 6.93661 21.7473 9.41498 21.75 12ZM20.25 12C20.25 10.3683 19.7661 8.77325 18.8596 7.41655C17.9531 6.05984 16.6646 5.00242 15.1571 4.37799C13.6497 3.75357 11.9909 3.59019 10.3905 3.90852C8.79017 4.22685 7.32016 5.01259 6.16637 6.16637C5.01259 7.32015 4.22685 8.79016 3.90853 10.3905C3.5902 11.9908 3.75358 13.6496 4.378 15.1571C5.00242 16.6646 6.05984 17.9531 7.41655 18.8596C8.77326 19.7661 10.3683 20.25 12 20.25C14.1873 20.2475 16.2843 19.3775 17.8309 17.8309C19.3775 16.2843 20.2475 14.1873 20.25 12Z"
-                                  fill="#06ff00"
-                                />
-                              </svg>
-                            </div>
-                            <div>
-                              {t(
-                                'pay_nothing_for_the_first_7_days',
-                                'Pay nothing for the first 7 days'
-                              )}
-                            </div>
-                          </div>
-                          <div className="flex gap-[5px] items-center">
-                            <div>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                              >
-                                <path
-                                  d="M16.2806 9.21937C16.3504 9.28903 16.4057 9.37175 16.4434 9.46279C16.4812 9.55384 16.5006 9.65144 16.5006 9.75C16.5006 9.84856 16.4812 9.94616 16.4434 10.0372C16.4057 10.1283 16.3504 10.211 16.2806 10.2806L11.0306 15.5306C10.961 15.6004 10.8783 15.6557 10.7872 15.6934C10.6962 15.7312 10.5986 15.7506 10.5 15.7506C10.4014 15.7506 10.3038 15.7312 10.2128 15.6934C10.1218 15.6557 10.039 15.6004 9.96938 15.5306L7.71938 13.2806C7.57865 13.1399 7.49959 12.949 7.49959 12.75C7.49959 12.551 7.57865 12.3601 7.71938 12.2194C7.86011 12.0786 8.05098 11.9996 8.25 11.9996C8.44903 11.9996 8.6399 12.0786 8.78063 12.2194L10.5 13.9397L15.2194 9.21937C15.289 9.14964 15.3718 9.09432 15.4628 9.05658C15.5538 9.01884 15.6514 8.99941 15.75 8.99941C15.8486 8.99941 15.9462 9.01884 16.0372 9.05658C16.1283 9.09432 16.211 9.14964 16.2806 9.21937ZM21.75 12C21.75 13.9284 21.1782 15.8134 20.1068 17.4168C19.0355 19.0202 17.5127 20.2699 15.7312 21.0078C13.9496 21.7458 11.9892 21.9389 10.0979 21.5627C8.20656 21.1865 6.46928 20.2579 5.10571 18.8943C3.74215 17.5307 2.81355 15.7934 2.43735 13.9021C2.06114 12.0108 2.25422 10.0504 2.99218 8.26884C3.73013 6.48726 4.97982 4.96451 6.58319 3.89317C8.18657 2.82183 10.0716 2.25 12 2.25C14.585 2.25273 17.0634 3.28084 18.8913 5.10872C20.7192 6.93661 21.7473 9.41498 21.75 12ZM20.25 12C20.25 10.3683 19.7661 8.77325 18.8596 7.41655C17.9531 6.05984 16.6646 5.00242 15.1571 4.37799C13.6497 3.75357 11.9909 3.59019 10.3905 3.90852C8.79017 4.22685 7.32016 5.01259 6.16637 6.16637C5.01259 7.32015 4.22685 8.79016 3.90853 10.3905C3.5902 11.9908 3.75358 13.6496 4.378 15.1571C5.00242 16.6646 6.05984 17.9531 7.41655 18.8596C8.77326 19.7661 10.3683 20.25 12 20.25C14.1873 20.2475 16.2843 19.3775 17.8309 17.8309C19.3775 16.2843 20.2475 14.1873 20.25 12Z"
-                                  fill="#06ff00"
-                                />
-                              </svg>
-                            </div>
-                            <div>
-                              {t(
-                                'cancel_anytime_hassle_free',
-                                'Cancel anytime, hassle-free'
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                    <BillingComponent />
-                  </>
+                  <BillingComponent />
                 ) : (
-                  <>
-                    <Title />
-                    <div className="flex flex-1 flex-col">{children}</div>
-                  </>
+                  <div className="flex flex-1 flex-col">{children}</div>
                 )}
               </div>
             </div>
