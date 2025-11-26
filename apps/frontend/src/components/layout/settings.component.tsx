254ec5a60b1e4f94d3dc77fd2dea78136896ad61
Gharib_Shaban26 <165028759+Gcodesh@users.noreply.github.com>
2025-11-27 01:20:12 +0200

    Add Public API tab conditionally in settings

--- apps/frontend/src/components/layout/settings.component.tsx
+++ apps/frontend/src/components/layout/settings.component.tsx
@@ -103,7 +103,9 @@ export const SettingsPopup: FC<{
     if (user?.tier.current !== 'FREE') {
       arr.push({ tab: 'signatures', label: t('signatures', 'Signatures') });
     }
-   
+    if (user?.tier?.public_api && isGeneral && showLogout) {
+      arr.push({ tab: 'api', label: t('public_api', 'Public API') });
+    }
 
     return arr;
   }, [user, isGeneral, showLogout, t]);
@@ -192,7 +194,14 @@ export const SettingsPopup: FC<{
                 </div>
               )}
 
-             
+              {tab === 'api' &&
+                !!user?.tier?.public_api &&
+                isGeneral &&
+                showLogout && (
+                  <div>
+                    <PublicComponent />
+                  </div>
+                )}
             </div>
           </form>
         </FormProvider>
